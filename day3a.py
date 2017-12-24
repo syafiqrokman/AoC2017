eeddb9004ac44c1146246d0ab024fa4b4343c126
Syafiq Rokman <msyafiq.rokman@gmail.com>
2017-12-24 23:45:40 +0800

    ideas for day3a

---
+++ day3a.py
@@ -0,0 +1,8 @@
+1.create map/env 
+2. 
+manhattan distance formula / distance between (p1,p2) and (q1,q2): |p1-q1| + |p2 - q2| 
+
+
+
+1. create  dict(memory_position=coordinate) 
+2. |p1-q1| + |p2 - q2| 
